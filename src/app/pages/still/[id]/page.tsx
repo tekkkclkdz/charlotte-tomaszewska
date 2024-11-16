cb027ced4df5b8dc8ba195a4866d0e4488088e9d
Tekkkclkdz <antekkorzecc@gmail.com>
2024-11-16 17:10:06 +0100

    Final day V2

--- src/app/pages/still/[id]/page.tsx
+++ src/app/pages/still/[id]/page.tsx
@@ -52,56 +52,59 @@ export default function ProjectDetail({ params }: { params: Params }) {
     return <div>Projekt nie został znaleziony.</div>;
   }
 
+  // Safely access additionalImages and filter out undefined entries
+  const additionalImages = project.additionalImages?.filter((pic): pic is ImageType => pic != null) || [];
+
   return (
     <div className="p-4 text-center bg-white">
-  {/* Container for Navbar and Logo */}
-  <div className="sticky top-0 mix-blend-difference z-50 ">
-    <NavBar stillOrMoving={0} intro={1} />
-  </div>
-  <div className="flex justify-center">
-    <Link href={"/"}>
-      <Image src={logo} alt="logo" width={120} height={120} />
-    </Link>
-  </div>
-  {/* Project Content */}
-  <div className="w-full h-full left-0 top-0 border-none shadow-none" suppressHydrationWarning={true}>
-    {/* Grid container with conditional width using sm: classes */}
-    <div className={`grid gap-2 bg-white sm:grid-cols-2 sm:w-9/12 grid-cols-1 mx-auto`}>
-      {project.additionalImages?.map((pics, idx) => (
-        <div
-          key={pics.src}
-          className={isHorizontal(pics) ? 'col-span-2' : 'col-span-1'}
-        >
-          <Image
-            src={pics.src}
-            alt="placeholder"
-            loading="lazy"
-            className="transition duration-150 hover:opacity-75 cursor-pointer my-2 border-none w-full"
-            width={pics.width}
-            height={pics.height}
-            onClick={() => {
-              lightboxRef.current?.openGallery(idx);
-            }}
-          />
+      {/* Container for Navbar and Logo */}
+      <div className="sticky top-0 mix-blend-difference z-50 ">
+        <NavBar stillOrMoving={0} intro={1} />
+      </div>
+      <div className="flex justify-center">
+        <Link href={"/"}>
+          <Image src={logo} alt="logo" width={120} height={120} />
+        </Link>
+      </div>
+      {/* Project Content */}
+      <div className="w-full h-full left-0 top-0 border-none shadow-none" suppressHydrationWarning={true}>
+        {/* Grid container with conditional width using sm: classes */}
+        <div className={`grid gap-2 bg-white sm:grid-cols-2 sm:w-9/12 grid-cols-1 mx-auto`}>
+          {additionalImages.map((pics, idx) => (
+            <div
+              key={pics.src}
+              className={isHorizontal(pics) ? 'col-span-2' : 'col-span-1'}
+            >
+              <Image
+                src={pics.src}
+                alt="placeholder"
+                loading="lazy"
+                className="transition duration-150 hover:opacity-75 cursor-pointer my-2 border-none w-full"
+                width={pics.width}
+                height={pics.height}
+                onClick={() => {
+                  lightboxRef.current?.openGallery(idx);
+                }}
+              />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
 
-    <LightGalleryComponent
-      onInit={(ref) => {
-        if (ref) {
-          lightboxRef.current = ref.instance;
-        }
-      }}
-      speed={500}
-      plugins={[lgZoom, lgThumbnail]}
-      dynamic
-      download={false}
-      dynamicEl={project.additionalImages?.map((allImg) => ({
-        src: allImg.src,
-      }))}
-    />
-  </div>
-</div>
+        <LightGalleryComponent
+          onInit={(ref) => {
+            if (ref) {
+              lightboxRef.current = ref.instance;
+            }
+          }}
+          speed={500}
+          plugins={[lgZoom, lgThumbnail]}
+          dynamic
+          download={false}
+          dynamicEl={additionalImages.map((allImg) => ({
+            src: allImg.src,
+          }))}
+        />
+      </div>
+    </div>
   );
 }

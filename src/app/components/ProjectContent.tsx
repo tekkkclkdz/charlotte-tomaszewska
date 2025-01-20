d58d21f4e7810e7447d713db99074b9605fe5559
Tekkkclkdz <antekkorzecc@gmail.com>
2025-01-20 19:29:49 +0100

    Text for ig web

--- src/app/components/ProjectContent.tsx
+++ src/app/components/ProjectContent.tsx
@@ -16,31 +16,26 @@ type ProjectsArray = Project[];
 
 const ProjectContent = ({ projects }: { projects: ProjectsArray }) => {
   const [activeProject, setActiveProject] = useState<number | null>(null);
-  const [isAtBottom, setIsAtBottom] = useState(false);  // Stan, który będzie odpowiadał za ukrycie ostatniego tytułu
-  const [isMobile, setIsMobile] = useState(false);  // Stan określający, czy użytkownik jest na urządzeniu mobilnym
-  const [isLogoVisible, setIsLogoVisible] = useState(true); // Stan odpowiadający za widoczność logo
+  const [isAtBottom, setIsAtBottom] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [isLogoVisible, setIsLogoVisible] = useState(true);
+  const [isInstagram, setIsInstagram] = useState(false); // Stan dla przeglądarki Instagram
   const logoRef = useRef<HTMLDivElement | null>(null);
 
   const handleResize = () => {
-    setIsMobile(window.innerWidth <= 768);  // Zmieniamy na true, jeśli szerokość ekranu to urządzenie mobilne
+    setIsMobile(window.innerWidth <= 768);
   };
 
   const handleScroll = () => {
     const scrollPosition = window.scrollY + window.innerHeight;
     const documentHeight = document.body.offsetHeight;
 
-    // Sprawdzanie, czy użytkownik zjechał na sam dół strony
-    const atBottom = scrollPosition >= documentHeight - (isMobile ? 100 : 10); // Dla mobile zwiększamy próg
+    const atBottom = scrollPosition >= documentHeight - (isMobile ? 100 : 10);
     setIsAtBottom(atBottom);
 
-    // Jeśli nie jest to urządzenie mobilne, ukrywamy logo po przewinięciu strony
     if (!isMobile && logoRef.current) {
       const logoPosition = logoRef.current.getBoundingClientRect().top + window.scrollY;
-      if (window.scrollY > logoPosition) {
-        setIsLogoVisible(false);
-      } else {
-        setIsLogoVisible(true);
-      }
+      setIsLogoVisible(window.scrollY <= logoPosition);
     }
 
     let currentActiveProject: number | null = null;
@@ -62,11 +57,16 @@ const ProjectContent = ({ projects }: { projects: ProjectsArray }) => {
   };
 
   useEffect(() => {
-    handleResize(); // Początkowa inicjalizacja stanu dla urządzeń mobilnych
-    window.addEventListener("resize", handleResize); // Obsługa zmiany rozmiaru okna
-    window.addEventListener("scroll", handleScroll); // Obsługa przewijania strony
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("scroll", handleScroll);
+
+    // Wykrywanie przeglądarki Instagram
+    const userAgent = navigator.userAgent || navigator.vendor;
+    setIsInstagram(/Instagram/.test(userAgent));
+
     return () => {
-      window.removeEventListener("resize", handleResize); // Usunięcie nasłuchiwaczy
+      window.removeEventListener("resize", handleResize);
       window.removeEventListener("scroll", handleScroll);
     };
   }, [projects]);
@@ -80,7 +80,7 @@ const ProjectContent = ({ projects }: { projects: ProjectsArray }) => {
           <Element key={project.id} name={`project${project.id}`}>
             <div
               className={`relative flex items-center justify-center bg-white text-black font-light z-0 ${
-                index !== 0 ? "py-8" : "mb-8" /* No padding for the first project */
+                index !== 0 ? "py-8" : "mb-8"
               }`}
               id={`project${project.id}`}
             >
@@ -94,10 +94,11 @@ const ProjectContent = ({ projects }: { projects: ProjectsArray }) => {
         <div>No projects available</div>
       )}
 
-      {/* Bottom logo */}
       <div
         ref={logoRef}
-        className={`relative flex items-center justify-center bg-white pb-20 transition-opacity duration-300 ease-in-out ${isLogoVisible ? 'opacity-100' : 'opacity-0'}`}
+        className={`relative flex items-center justify-center bg-white pb-20 transition-opacity duration-300 ease-in-out ${
+          isLogoVisible ? "opacity-100" : "opacity-0"
+        }`}
       >
         <Image
           src={logo}
@@ -106,12 +107,13 @@ const ProjectContent = ({ projects }: { projects: ProjectsArray }) => {
         />
       </div>
 
-      {/* Active project title display */}
       <div className="fixed bottom-0 left-0 right-0 mix-blend-difference shadow-md text-center transition-opacity duration-300 ease-in-out">
         <div className="absolute inset-0 opacity-90 -z-10"></div>
         {activeProject !== null && !isAtBottom ? (
           <h2
-            className={`relative text-lg sm:text-2xl mb-[7px] sm:mb-2 font-customMedium mix-blend-difference text-black dark:text-white opacity-100 transition-opacity duration-300`}
+            className={`relative text-lg sm:text-2xl ${
+              isInstagram ? "mb-[7px]" : "mb-1"
+            } font-customMedium mix-blend-difference text-black dark:text-white opacity-100 transition-opacity duration-300`}
           >
             {projects.find((p) => p.id === activeProject)?.title ||
               projects[projects.length - 1]?.title}
--- src/app/pages/still/[id]/page.tsx
+++ src/app/pages/still/[id]/page.tsx
@@ -1,10 +1,8 @@
-'use client';
-
+"use client"
 import { useEffect, useState, useRef } from 'react';
 import Image, { StaticImageData } from 'next/image';
 import { useMediaQuery } from 'react-responsive';
 import Masonry from 'react-masonry-css';
-
 import type { LightGallery } from 'lightgallery/lightgallery';
 import LightGalleryComponent from 'lightgallery/react';
 import 'lightgallery/css/lightgallery.css';
@@ -12,13 +10,10 @@ import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import lgZoom from 'lightgallery/plugins/zoom';
-
 import { stillProjects } from './../../../stillProjects';
 import NavBar from '@/app/components/NavBar2'; // Importing NavBar
-
 import logo from './../../../../../public/charlotte heart black.png';
 import Link from 'next/link';
-
 import ContactBioBar from '@/app/components/ContactBioBar';
 
 type Params = {
@@ -47,8 +42,15 @@ export default function ProjectDetail({ params }: { params: Params }) {
   const project = stillProjects.find((project) => project.id === parseInt(id, 10));
 
   const isGif = (pic: ImageType) => pic.src.endsWith('.gif');
-
   const isHorizontal = (pic: ImageType) => pic.width > pic.height;
+
+  const [isInstagram, setIsInstagram] = useState(false);
+
+  useEffect(() => {
+    // Wykrywanie przeglądarki Instagram
+    const userAgent = navigator.userAgent || navigator.vendor;
+    setIsInstagram(/Instagram/.test(userAgent));
+  }, []);
 
   if (!project) {
     return <div>Projekt nie został znaleziony.</div>;
@@ -57,14 +59,13 @@ export default function ProjectDetail({ params }: { params: Params }) {
   type ImageType = StaticImageData;
 
   const additionalImages: ImageType[] = (project.additionalImages || [])
-  .filter((pic): pic is StaticImageData => !!pic) // Usuwa undefined
-  .map((pic) => ({
-    src: typeof pic === 'object' && 'src' in pic ? pic.src : '',
-    width: pic?.width || 0,
-    height: pic?.height || 0,
-  }))
-  .filter((img): img is ImageType => img.src !== '');
-
+    .filter((pic): pic is StaticImageData => !!pic) // Usuwa undefined
+    .map((pic) => ({
+      src: typeof pic === 'object' && 'src' in pic ? pic.src : '',
+      width: pic?.width || 0,
+      height: pic?.height || 0,
+    }))
+    .filter((img): img is ImageType => img.src !== '');
 
   return (
     <div className="text-center bg-white">
@@ -72,14 +73,9 @@ export default function ProjectDetail({ params }: { params: Params }) {
         <NavBar stillOrMoving={2} intro={1} />
         <ContactBioBar intro={1} underline={0} />
       </div>
-  
-      <div
-        className="w-full h-full left-0 top-0 border-none shadow-none"
-        suppressHydrationWarning={true}
-      >
-        <div
-          className={`grid gap-2 bg-white sm:grid-cols-2 sm:w-9/12 grid-cols-1 mx-auto px-2`}
-        >
+
+      <div className="w-full h-full left-0 top-0 border-none shadow-none" suppressHydrationWarning={true}>
+        <div className={`grid gap-2 bg-white sm:grid-cols-2 sm:w-9/12 grid-cols-1 mx-auto px-2`}>
           {additionalImages.map((pics, idx) => (
             <div
               key={pics.src}
@@ -108,7 +104,7 @@ export default function ProjectDetail({ params }: { params: Params }) {
             </div>
           ))}
         </div>
-  
+
         {/* Render LightGalleryComponent only on desktops */}
         {!isMobile && (
           <LightGalleryComponent
@@ -126,13 +122,18 @@ export default function ProjectDetail({ params }: { params: Params }) {
           />
         )}
       </div>
-  
+
+      {/* Render credits with different margins for Instagram browser */}
       {project.credits && (
-        <div className="z-50 fixed bottom-0 left-1/2 transform -translate-x-1/2 sm:w-9/12 w-full sm:px-0 px-4 text-sm sm:text-2xl mb-0 sm:mb-2 font-customMedium mix-blend-difference text-black dark:text-white">
+        <div
+          className={`z-50 fixed bottom-0 left-1/2 transform -translate-x-1/2 sm:w-9/12 w-full sm:px-0 px-4 text-sm sm:text-2xl font-customMedium mix-blend-difference text-black dark:text-white ${
+            isInstagram ? 'mb-[7px]' : 'mb-1'
+          }`}
+        >
           {project.credits}
         </div>
       )}
-  
+
       {/* Logo below the last image */}
       <div className="flex justify-center">
         <Link href={'/'}>
@@ -148,8 +149,3 @@ export default function ProjectDetail({ params }: { params: Params }) {
     </div>
   );
 }
-
-
-//className={isHorizontal(pics) ? 'col-span-2 px-2 sm:px-0' : 'col-span-1 px-0 sm:px-0'}
-
-
